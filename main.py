fcb389ef50ab0cef60f9a5b68d4ca7974317c2b6
Z3N <59246899+Z3-N1X@users.noreply.github.com>
2025-02-10 21:00:10 +0330

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/", include_in_schema=False)
+@app.head("/")
 def read_root():
     return {"message": "API is running on Render!"}
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT"))  # Default to 8000 locally
+    port = int(os.environ.get("PORT", 8000))  # Use Render's port
     uvicorn.run(app, host="0.0.0.0", port=port)
