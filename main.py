674ec7a52204613a283652af67f9c1851c0a6644
Z3N <59246899+Z3-N1X@users.noreply.github.com>
2025-02-10 20:57:30 +0330

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ def read_root():
     return {"message": "API is running on Render!"}
 
 if __name__ == "__main__":
-    port = int(10000)  # Default to 8000 locally
+    port = int(os.environ.get("PORT"))  # Default to 8000 locally
     uvicorn.run(app, host="0.0.0.0", port=port)
